771b8739d4b46354a983602ad96f755322c610c0
Ardi Hyseni <ardihyseni737@gmail.com>
2026-09-13 17:20:16 +0200

    Add CineSrc Cast icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -232,3 +232,13 @@ export const PictureInPictureIcon = (props: IconProps) => (
     <rect x="12" y="11" width="7" height="6" rx="1" />
   </IconBase>
 );
+
+
+export const CastIcon = (props: IconProps) => (
+  <IconBase {...props}>
+    <path d="M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6" />
+    <path d="M2 12a9 9 0 0 1 8 8" />
+    <path d="M2 16a5 5 0 0 1 4 4" />
+    <path d="M2 20h.01" />
+  </IconBase>
+);
