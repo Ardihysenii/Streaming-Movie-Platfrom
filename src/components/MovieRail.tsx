1eda6fd9ab3ec2d35d3b5e0934600136cba061f7
Ardi Hyseni <ardihyseni737@gmail.com>
2026-09-26 01:13:46 +0200

    Restore homepage design before redesign pass

--- src/components/MovieRail.tsx
+++ src/components/MovieRail.tsx
@@ -92,8 +92,8 @@ export function ForYouRail({ movies }: { movies: Movie[] }) {
   return (
     <ChoiceRail
       className="for-you-rail"
-      eyebrow="A considered selection"
-      title="Recommended"
+      eyebrow="Made for your next watch"
+      title="For You"
       choices={[
         { label: "Movies", movies: movies.filter((movie) => movie.media_type !== "tv"), href: "/movies/" },
         { label: "TV Shows", movies: movies.filter((movie) => movie.media_type === "tv"), href: "/series/" },
