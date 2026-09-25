e04cc47502d60670146fe156c1d87bb94b6038c9
Ardi Hyseni <ardihyseni737@gmail.com>
2026-09-25 12:46:21 +0200

    Allow MediUX artwork assets

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "api.mediux.pro",
+      },
+      {
+        protocol: "https",
         hostname: "assets.fanart.tv",
       },
       {
